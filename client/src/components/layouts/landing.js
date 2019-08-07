5416f43693abccb778e949639548adb80af1a085
Robert Logan <bigdog_41@hotmail.com>
2019-08-07 03:49:00 -0700

    landing page

---
+++ client/src/components/layouts/landing.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import NavBar from "./navBar";
+import Vid from "../../assets/images/twiz (1).mp4";
+//import Typography from "@material-ui/core/Typography";
+import "./landing.css";
+import VideoCover from "react-video-cover";
+
+export class landing extends Component {
+  render() {
+    const videoOptions = {
+      src: Vid,
+      autoPlay: true,
+      loop: true,
+      muted: true
+    };
+    return (
+      <div
+        style={{
+          overflow: "hidden"
+          //top: "0",
+          //left: "0",
+        }}
+      >
+        <VideoCover
+          videoOptions={videoOptions}
+          style={{ overflow: "hidden", position: "absolute" }}
+        />
+        <div
+          style={{
+            backgroundColor: "black",
+            opacity: 0.3,
+            width: "120vw",
+            height: "120vh",
+            position: "fixed"
+            //top: "-10vh",
+            //left: "-10vw",
+          }}
+        />
+        <NavBar />
+      </div>
+    );
+  }
+}
+
+export default landing;
+
+{
+  /* <video
+          className='videoTag'
+          autoPlay
+          loop
+          muted
+          style={{
+            top: "0",
+            left: "0",
+            objectFit: "cover",
+            // right: "0",
+            position: "absolute",
+            overFlow: "hidden",
+            height: "100%"
+            //flex: 1
+          }}
+        >
+          <source src={Vid} type='video/mp4' />
+        </video> */
+}
