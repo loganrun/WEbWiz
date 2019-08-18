3bdd4e9daeb456cc249a7cb2283ac8b38cd30094
loganrun <bigdog_41@hotmai.com>
2019-08-18 16:04:50 -0700

    landing added

--- client/src/components/layouts/landing.js
+++ client/src/components/layouts/landing.js
@@ -4,6 +4,9 @@ import Vid from "../../assets/images/twiz (1).mp4";
 import Typography from "@material-ui/core/Typography";
 import "./landing.css";
 import VideoCover from "react-video-cover";
+import { Container } from "@material-ui/core";
+import Button from '@material-ui/core/Button';
+import { Link } from "react-router-dom";
 
 export class landing extends Component {
   render() {
@@ -28,37 +31,54 @@ export class landing extends Component {
         <div
           style={{
             backgroundColor: "black",
-            opacity: 0.5,
-            width: "120vw",
-            height: "120vh",
+            opacity: 0.55,
+            width: "100vw",
+            height: "100vh",
             position: "fixed"
             //top: "-10vh",
             //left: "-10vw",
           }}
         >
-          <Typography
-            variant='h2'
-            style={{ color: "white", marginLeft: "150", marginTop: "150" }}
+          
+          {/* <Typography
+            variant='h2' align='center' 
+            style={{ color: "white", marginLeft: 150, marginTop: 150,zIndex: 1000000 }}
           >
             TIRED OF PEEING IN A CUP?
           </Typography>
-          <Typography variant='h3' style={{ color: "white" }}>
+          <Typography variant='h3' style={{ color: "white" }} align='center'>
             SIGN UP here AND FIND A BATHROOM NEAR YOU
-          </Typography>
+          </Typography> */}
         </div>
         <NavBar />
-
-        <div style={{ marginLeft: "150", marginTop: "150", zIndex: "1000000" }}>
+        <Container style={{overflow:'hidden', }}> 
+          <div style={{}}>
           <Typography
-            variant='h2'
-            style={{ color: "white", marginLeft: "150", marginTop: "150" }}
+            variant='h2' align='center'
+            paragraph
+            style={{ color: "white", position: 'relative', marginTop: 200, marginBottom: 100  }}
           >
-            TIRED OF PEEING IN A CUP?
+            Are you visiting a new city? 
+            A rideshare driver looking for a place to "go"? Click 
+            <Link
+            to='signup'
+            variant='body2'
+            style={{ color: "white"}}
+          ><Button variant='outlined'size ='large' style={{backgroundColor:'white', opacity: .9, marginLeft: 10, marginRight: 10 }}>HERE</Button></Link> 
+            and WHIZZ will find you the closest place too find relief!
           </Typography>
-          <Typography variant='h3' style={{ color: "white" }}>
-            SIGN UP here AND FIND A BATHROOM NEAR YOU
-          </Typography>
+          <Typography variant='h4' style={{ color: "white", position: 'relative', margintop: 50}} align='center'>
+            Add your business to our growing community by clicking
+            <Link
+            to='signup'
+            variant='body2'
+            style={{ color: "white"}}
+          ><Button variant='outlined'size ='small' style={{backgroundColor:'white', opacity: .9, marginLeft: 10, marginRight: 10 }}>HERE</Button></Link> 
+          </Typography> */}
         </div>
+        </Container>
+
+       
       </div>
     );
   }
--- client/src/components/layouts/navBar.js
+++ client/src/components/layouts/navBar.js
@@ -3,9 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-//import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 const useStyles = makeStyles(theme => ({
   root: {
@@ -26,27 +24,22 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position='static' elevation='0'>
         <Toolbar>
-          {/* <IconButton
-            edge='start'
-            className={classes.menuButton}
-            color='inherit'
-            aria-label='menu'
-          >
-            <MenuIcon />
-          </IconButton> */}
           <Typography variant='h6' className={classes.title}>
             WHIZZ
+          </Typography>
+          <Typography variant='h6'>
+          Business Partners Sign in
           </Typography>
           <Link
             to='login'
             variant='body2'
             style={{ color: "white", fontSize: 20 }}
           >
-            Business Partners Sign in here!
+            <Button variant='outlined'size ='medium' color= 'inerit'style={{backgroundColor:'white', opacity: .9, marginLeft: 10 }}>Here</Button>
           </Link>
         </Toolbar>
       </AppBar>
     </div>
   );
 }
-//<Button color='inherit'>Login</Button>
+
