2d3a0275541e143eb5c777699b51c8663f34ed5a
loganrun <bigdog_41@hotmai.com>
2019-09-26 09:54:31 -0700

    deploy prep

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require('path')
 
 const app = express();
 connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API running"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/bathrooms", require("./routes/api/bathroom"));
@@ -14,7 +14,7 @@ app.use("/api/restaurants", require("./routes/api/restaurant"));
 app.use("/api/bathReview", require("./routes/api/bathReview"));
 app.use("/api/restReview", require("./routes/api/restReview"));
 
-if (process.env.NODE.ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
