71a4eabfedc6a9721a2140521af0b0c9084bed64
Robert Logan <bigdog_41@hotmail.com>
2019-09-25 23:20:11 -0700

    server

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.use("/api/restaurants", require("./routes/api/restaurant"));
 app.use("/api/bathReview", require("./routes/api/bathReview"));
 app.use("/api/restReview", require("./routes/api/restReview"));
 
+if (process.env.NODE.ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
